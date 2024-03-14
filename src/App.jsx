27dbaf53768e30361942b88dbc9e87a7d896ad04
Hadiur Rahman <hadiurahman139@gmail.com>
2024-03-15 03:21:21 +0600

    succesfully fetch the fake json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Blogs from "./Component/Blogs/Blogs";
 import Header from "./Component/Header/Header";
 
 function App() {
-  return <>
-  <Header></Header>
-  </>;
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  );
 }
 
 export default App;
